6648dd236b962865c5c0106d9d0b1cdf3046c57b
TubbyTubTub <39911498+Aaylen@users.noreply.github.com>
2024-05-13 16:59:37 -0700

    link to resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,27 +1,39 @@
 import React from 'react';
-import { Image } from '@react-three/drei';
+import { Image, Text } from '@react-three/drei';
 import resume from '../images/resume.png';
 
 const Resume = () => {
+  const handleTitleClick = (link) => {
+    window.open(link, '_blank');
+  };
+
   return (
     <group position={[0, 0, 0.01]}>
-      {/* Resume image */}
-      <group position={[0, 0, 0.02]}>
-        <Image
-          url={resume}
-          material={{ transparent: true }}
-          scale={[0.7, 0.89, 1]}
-          anchorX="center"
-          anchorY="middle"
-        />
-      </group>
       {/* Background plane */}
       <mesh>
         <planeGeometry args={[0.9, 0.9]} />
         <meshBasicMaterial color="lightblue" />
       </mesh>
 
-      
+      {/* Resume image */}
+      <group position={[0, 0, 0.01]}>
+        <Image
+          url={resume}
+          material={{ transparent: true }}
+          scale={[0.7, 0.89, 1]}
+          anchorX="center"
+          anchorY="middle"
+          onClick={() => handleTitleClick("https://docs.google.com/document/d/1YPODad1EtTeHU8DBNTkAxas6wH0pEqDPU-50jft4wM4/edit?usp=sharing")}
+          onPointerOver={() => {document.body.style.cursor = 'pointer';}}
+        />
+      </group>
+
+      {/* Text */}
+      <group position={[0, 0, .02]}>
+        <Text fontSize={0.05} color={"black"} anchorX="center" anchorY="middle">
+          Click to View
+        </Text>
+      </group>
     </group>
   );
 };
